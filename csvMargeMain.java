5a94f6a5dbc1fd664cc16a34d2c25e66fc34ce95
mu-pan <choco03152@gmail.com>
2017-10-12 01:38:59 +0900

    ソート処理追加

--- csvMargeMain.java
+++ csvMargeMain.java
@@ -1,12 +1,14 @@
-package main;
+﻿package main;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PipedReader;
-import java.io.PipedWriter;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * @author u122092
@@ -22,26 +24,26 @@ public class csvMargeMain {
 
 		File file = new File("C:\\1000\\C16_CSV.csv");
 
+		// CSVを格納するリスト
+		List<String[]> seisanList = new ArrayList<String[]>();
+
 		try {
 			// ①CSVを読み込む
-			BufferedReader br = new BufferedReader(new FileReader(file));
+			// BufferedReader br = new BufferedReader(new FileReader(file));
+			//
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "SJIS"));
 
-			// ②PipedWriterオブジェクトpipeOutを生成
-			PipedWriter pipeOut = new PipedWriter();
-			// ③PipedReaderオブジェクトpipeInを生成
-			PipedReader pipeIn = new PipedReader(pipeOut);
-			// ④PrintWriterクラスでPipedWriterクラスの
-			// オブジェクトpipeOutをラップ
-			PrintWriter out = new PrintWriter(pipeOut);
-
+			// CSV読み込み用
 			String line;
 
 			// 一行ずつ読み込み
 			while ((line = br.readLine()) != null) {
-				String[] arrayline = line.split(",", 5); // 行をカンマ区切りで配列に変換
 
-				for (String elem : arrayline) {
-					System.out.println(elem);
+				String[] code = line.split(","); // 行をカンマ区切りで配列に変換
+
+				// 特定の値を除く（6項目：0002 タクシーを除く）
+				if (!code[5].equals("0006")) {
+					seisanList.add(code);
 				}
 			}
 			br.close();
@@ -50,13 +52,32 @@ public class csvMargeMain {
 			System.out.println(e);
 		}
 
-		// 特定の値を除く（6項目：0002 タクシーを除く）
-
 		// 明細データを部門単位でソート
+		Collections.sort(seisanList, new Comparator<String[]>() {
+			public int compare(String[] str1, String[] str2) {
+				return str1[1].compareTo((str2[1]));
+			}
+		});
+
+		String Bumon = null;
+		long kingaku = 0L;
 
 		// 部門単位に明細を集計
+		for (String[] strings : seisanList) {
+			if (Bumon.equals(strings[1])) {
+				// KOTSUHIKINGAKUを集計
+				kingaku = kingaku + Long.parseLong(strings[10]);
+				continue;
+			} else {
+				// 集計した値が閾値以上の場合のみ出力対象
+				if (kingaku > 100000) {
 
-		// 集計した値が閾値以上の場合のみ出力対象
+				}
+				Bumon = strings[1];
+				kingaku = 0L;
+			}
+
+		}
 
 		// 出力時は集計行を追加する
 	}
