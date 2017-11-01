0cd9966c0dd77cc73c89395111007361d771d320
mu-pan <choco03152@gmail.com>
2017-11-02 03:17:25 +0900

    例外処理追加

--- JavaTest/src/main/CsvMargeMain.java
+++ JavaTest/src/main/CsvMargeMain.java
@@ -1,9 +1,10 @@
 package main;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,7 +12,6 @@ import java.util.List;
  *
  */
 public class CsvMargeMain {
-	private static final Long KINGAKU = 10000L;
 
 	/**
 	 * @param args
@@ -26,55 +26,25 @@ public class CsvMargeMain {
 
 			ReadFile read = new ReadFile();
 			// CSVを格納するリスト
-			List<String[]> seisanList = read.readFile(file);
+			List<String[]> seisanList;
+			try {
+				// ファイルの読み込み
+				seisanList = read.readFile(file);
 
-			// 明細データを部門単位でソート
-			Collections.sort(seisanList, new Comparator<String[]>() {
-				public int compare(String[] str1, String[] str2) {
-					return str1[1].compareTo((str2[1]));
-				}
-			});
-
-			String bumon = "";
-			long kingaku = 0L;
-
-			// 明細行のリスト
-			LinkedList<String[]> outPutList = new LinkedList<String[]>();
-			// 部門単位に明細を集計
-			for (String[] strings : seisanList) {
-				// 部門が一致する場合、金額を集計
-				if (bumon.equals(strings[1])) {
-					// KOTSUHIKINGAKUを集計
-					kingaku += Long.parseLong(strings[10]);
-					// 明細行を格納
-					outPutList.add(strings);
-					//
-				} else {
-					// 集計した値が閾値以上の場合のみ出力対象
-					if (kingaku > KINGAKU) {
-						// 集計した明細行と合計行を出力する
-						for (String[] string : outPutList) {
-							// System.out.println(string);
-							System.out.println("部門：" + string[1] + "金額：" + string[10]);
-						}
-						System.out.println("部門：" + bumon + "合計金額：" + kingaku);
+				// 明細データを部門単位でソート
+				Collections.sort(seisanList, new Comparator<String[]>() {
+					public int compare(String[] str1, String[] str2) {
+						return str1[1].compareTo((str2[1]));
 					}
-					bumon = strings[1];
-					kingaku = Long.parseLong(strings[10]);
-					// クリア
-					outPutList.clear();
-					// 明細行を格納
-					outPutList.add(strings);
-				}
-			}
-			// 最後の集計行を出す
-			if (kingaku > KINGAKU) {
-				for (String[] string : outPutList) {
-					System.out.println("部門：" + string[1] + "金額：" + string[10]);
-				}
-				System.out.println("部門：" + bumon + "合計金額：" + kingaku);
+				});
+
+				// ソート後のCSVを出力
+				read.outPutKoutsuhi(seisanList);
+
+			} catch (UnsupportedEncodingException | FileNotFoundException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
 			}
 		}
 	}
-	// TODO Junitテスト
 }
--- JavaTest/src/main/ReadFile.java
+++ JavaTest/src/main/ReadFile.java
@@ -6,9 +6,12 @@ package main;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -17,20 +20,25 @@ import java.util.List;
  */
 public class ReadFile {
 
+	// 閾値
+	private static final Long KINGAKU = 10000L;
+
 	/**
 	 * ファイルの読み込み
 	 *
 	 * @param file
 	 * @return seisanList
+	 * @throws FileNotFoundException
+	 * @throws UnsupportedEncodingException
 	 */
-	protected List<String[]> readFile(File file) {
+	protected List<String[]> readFile(File file) throws UnsupportedEncodingException, FileNotFoundException {
 
 		List<String[]> seisanList = new ArrayList<String[]>();
 
+		// ①CSVを読み込む(文字コードの指定）
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "SJIS"));
 		try {
 
-			// ①CSVを読み込む(文字コードの指定）
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "SJIS"));
 			// CSV読み込み用
 			String line;
 			// ヘッダー行を読み込み
@@ -44,13 +52,64 @@ public class ReadFile {
 					seisanList.add(code);
 				}
 			}
-			br.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		return seisanList;
 
+	}
+
+	/**
+	 *
+	 * @param seisanList
+	 */
+	protected void outPutKoutsuhi(List<String[]> seisanList) {
+
+		String bumon = "";
+		long kingaku = 0L;
+
+		// 明細行のリスト
+		LinkedList<String[]> outPutList = new LinkedList<String[]>();
+		// 部門単位に明細を集計
+		for (String[] strings : seisanList) {
+			// 部門が一致する場合、金額を集計
+			if (bumon.equals(strings[1])) {
+				// KOTSUHIKINGAKUを集計
+				kingaku += Long.parseLong(strings[10]);
+				// 明細行を格納
+				outPutList.add(strings);
+				//
+			} else {
+				// 集計した値が閾値以上の場合のみ出力対象
+				if (kingaku > KINGAKU) {
+					// 集計した明細行と合計行を出力する
+					for (String[] string : outPutList) {
+						// System.out.println(string);
+						System.out.println("部門：" + string[1] + "金額：" + string[10]);
+					}
+					System.out.println("部門：" + bumon + "合計金額：" + kingaku);
+				}
+				bumon = strings[1];
+				kingaku = Long.parseLong(strings[10]);
+				// クリア
+				outPutList.clear();
+				// 明細行を格納
+				outPutList.add(strings);
+			}
+		}
+		// 最後の集計行を出す
+		if (kingaku > KINGAKU) {
+			for (String[] string : outPutList) {
+				System.out.println("部門：" + string[1] + "金額：" + string[10]);
+			}
+			System.out.println("部門：" + bumon + "合計金額：" + kingaku);
+		}
 	}
 }
